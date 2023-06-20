ecfc8009699c230de4074a619ae9cf1204d1f8a6
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2023-06-20 19:30:03 -0300

    feat: add sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,86 @@
+<script setup>
+import logo from "@/assets/images/logo.png"
+</script>
+
+<template>
+  <aside collapsed>
+    <div class="logo">
+      <img v-bind:src="logo" alt="BoardCamp Logo" />
+    </div>
+
+    <button @click="$router.push('/rentals')" class="menu-item">
+      <font-awesome-icon icon="fa-solid fa-rectangle-list" />
+      Aluguéis
+    </button>
+
+    <button @click="$router.push('/customers')" class="menu-item">
+      <font-awesome-icon icon="fa-solid fa-user-group" />
+      Clientes
+    </button>
+
+    <button @click="$router.push('/games')" class="menu-item">
+      <font-awesome-icon icon="fa-solid fa-chess-board" />
+      Jogos
+    </button>
+  </aside>
+</template>
+
+<style scoped>
+aside {
+  height: 100vh;
+  overflow-y: auto;
+  overflow-x: hidden;
+
+  width: var(--sidebar-width);
+  background-color: var(--colors-main);
+  color: #fff;
+  padding: 20px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex-shrink: 0;
+
+  position: sticky;
+  top: 0;
+  z-index: 2;
+}
+
+div.logo {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+  margin-bottom: 10px;
+}
+
+button.menu-item {
+  cursor: pointer;
+  border-radius: 4px;
+  width: 252px;
+  border: none;
+  padding: 16px 16px 16px 45px;
+  margin-top: 10px;
+  background-color: rgba(255, 255, 255, 0.15);
+  color: white;
+  font-size: 1.1em;
+
+  word-break: break-all;
+
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  position: relative;
+}
+
+button.menu-item:hover {
+  background-color: rgba(255, 255, 255, 0.3);
+}
+
+button.menu-item svg {
+  position: absolute;
+  left: 16px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,15 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
 
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { faUserGroup, faRectangleList, faChessBoard } from "@fortawesome/free-solid-svg-icons"
+
+library.add(faUserGroup, faRectangleList, faChessBoard)
+
 const app = createApp(App)
+
+app.component("font-awesome-icon", FontAwesomeIcon)
 
 app.use(router)
 
