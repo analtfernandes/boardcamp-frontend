fcfd94418a5edf12266ea0c607744c71aff089cb
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2023-06-23 14:48:49 -0300

    feat: add page container and title

---
+++ src/components/PageContainer.vue
@@ -0,0 +1,28 @@
+<script setup>
+import PageTitle from "@/components/PageTitle.vue";
+
+const props = defineProps(["title"]);
+</script>
+
+<template>
+  <div class="page">
+    <PageTitle>
+      {{ props.title }}
+    </PageTitle>
+    <div class="page-container">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+div.page {
+  width: calc(100% - var(--sidebar-width));
+  flex-shrink: 0;
+  background-color: #fafafa;
+}
+
+div.page-container {
+  padding: 20px;
+}
+</style>
---
+++ src/components/PageTitle.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="title-container">
+    <h1>
+      <slot />
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+div.title-container {
+  position: sticky;
+  top: 0px;
+  background-image: linear-gradient(to right, rgb(72, 61, 139), rgb(79, 67, 153));
+  width: 100%;
+  padding: 120px 20px 0;
+  z-index: 2;
+}
+
+h1 {
+  font-size: 2.4em;
+  font-weight: bold;
+  letter-spacing: 1px;
+  width: 100%;
+  padding-bottom: 12px;
+  border-bottom: 3px solid var(--colors-main);
+  color: #fff;
+  text-transform: uppercase;
+}
+</style>
