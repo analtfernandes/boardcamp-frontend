37e606ccfb10482376cad0f1a326cd28134c77c8
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2023-06-24 17:39:58 -0300

    refactor: remove unused route in app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 import Sidebar from "./components/Sidebar.vue";
 </script>
 
 <template>
   <main>
     <Sidebar />
-
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-    </nav>
-
     <RouterView />
   </main>
 </template>
