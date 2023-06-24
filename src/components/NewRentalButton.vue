b97c99056712db828e580f8bd317ad75a7b5a41e
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2023-06-24 14:22:05 -0300

    feat: add new rental button component

---
+++ src/components/NewRentalButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="new-rental" @click="$router.push('/rentals/new')" title="Alugar Jogo">
+    <font-awesome-icon icon="fa-solid fa-hand-holding-heart" />
+  </button>
+</template>
+
+<style scoped>
+button.new-rental {
+  width: 64px;
+  height: 64px;
+  position: fixed;
+  right: 24px;
+  bottom: 24px;
+  border-radius: 50%;
+  background-color: rgb(53, 121, 220);
+  color: #fff;
+  font-size: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.4);
+  cursor: pointer;
+  z-index: 10;
+}
+
+button.new-rental:hover {
+  background-color: rgb(44, 97, 176);
+}
+</style>
--- src/config/icon.js
+++ src/config/icon.js
@@ -1,8 +1,13 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-import { faUserGroup, faRectangleList, faChessBoard } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUserGroup,
+  faRectangleList,
+  faChessBoard,
+  faHandHoldingHeart
+} from "@fortawesome/free-solid-svg-icons";
 
-library.add(faUserGroup, faRectangleList, faChessBoard);
+library.add(faUserGroup, faRectangleList, faChessBoard, faHandHoldingHeart);
 
 export { FontAwesomeIcon };
